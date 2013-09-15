e76ab51bd63f10fe44668de641215a4950107c9d
Jonny Yu <yingshen.yu@gmail.com>
2013-09-15 21:30:10 +0800

    add support for --host option to specify the tracker servers in command line.
    instead of go on messing the handcrafted command line argument parsing, I've took time to look for a right solution to parse command line args.
    After look at various libs, I found NDesk.options + ManaConsole is my best shot.
    NDesk.Options is a powerful command line arg parser lib, it is getopt() style and using callbacks to handle each argument. The only short coming is it doesn't support
    latest 'git' style sub commands, here is where ManaConsole comes to help. ManyConsole is base on NDesk.Options and adding a wrapper framework for defining sub commands.
    each sub command is a derived class of ConsoleCommand. in each command class, you define the options same as NDesk.Options.
    You can define shared options between sub commands via defining a common base command class.
    
    I've also used NuGet to manage the 2 dependencies, it is very convenient, I can add and remove libraries without many dirty work.
    When you compile this project from fresh download, you need "Restore NuGet Packages" first, or it will missing assembly references.
    
    I hope someday C++ world has same NuGet thing. Just Kidding...

--- FastDFSClient/Program.cs
+++ FastDFSClient/Program.cs
@@ -1,234 +1,281 @@
-﻿using System.Collections.Generic;
-using System.Net;
-using FastDFS.Client;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
-using System.Text.RegularExpressions;
-using System;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
+using FastDFS.Client;
+using ManyConsole;
 
 namespace FastDFS.ConsoleApp
 {
+	abstract class FastDFSCommand : ConsoleCommand
+	{
+		protected int _timeout = 300;
+		protected List<IPEndPoint> _trackers = new List<IPEndPoint>();
+		protected bool _dryrun = false;
 
-    class Program
-    {
-        static int Main(string[] args)
-        {
-            if (args.Length < 2)
-            {
-                Usage();
-                return 1;
-            }
+		protected FastDFSCommand()
+		{
+			HasOption("t|timeout=",
+			          "timeout in seconds.",
+			          (int v) => _timeout = v);
+			HasOption ("h|host=", "tracker server in address:port format. Can add multiple times. ",
+			           v => _trackers.Add(CreateEndPoint(v)) );
+			HasOption ("dryrun", "dryrun only",
+			           v => _dryrun = true);
+		}
 
-            try
-            {
-                List<IPEndPoint> trackers = TrackersFromSettings();
-                ConnectionManager.Initialize(trackers);
+		private List<IPEndPoint> TrackersFromSettings()
+		{
+			List<IPEndPoint> trackers = new List<IPEndPoint>();
 
-                string op = args[0];
-                if (string.Compare(op, "-u") == 0 ||
-                    string.Compare(op, "--upload") == 0)
-                {
-                    StorageNode node = FastDFSClient.GetStorageNode("group1");
-                    string fileid = DoUpload(node, args[1]);
-                    Console.WriteLine("{0}", fileid);
-                    Console.WriteLine("http://{0}/{1}", trackers[0].Address.ToString(), fileid);
-                }
-                else if (string.Compare(op, "-d") == 0 ||
-                         string.Compare(op, "--download") == 0)
-                {
-                    int timeoutSecs = 300;
-                    string fildid = null;
-                    string destFileName = null;
+			foreach(string key in ConfigurationManager.AppSettings)
+			{
+				if (Regex.IsMatch(key, "Tracker[0-9]"))
+				{
+					trackers.Add(CreateEndPoint(ConfigurationManager.AppSettings[key]));
+				}
+			}
+			return trackers;
+		}
 
-                    int argIndex = 1;
-                    if (string.Compare(args[argIndex], "--timeout") == 0)
-                    {
-                        argIndex++;
-                        timeoutSecs = int.Parse(args[argIndex]);
-                        argIndex++;
-                    }
+		private IPEndPoint CreateEndPoint(string ipAndPort)
+		{
+			int pos = ipAndPort.IndexOf(':');
 
-                    fildid = args[argIndex++];
+			if (pos < 0)
+			{
+				// assume the default port if port isn't specified
+				return new IPEndPoint(IPAddress.Parse(ipAndPort), 22122);
+			}
 
-                    if (args.Length > argIndex)
-                    {
-                        destFileName = args[argIndex];
-                    }
+			int port = int.Parse(ipAndPort.Substring(pos + 1));
+			return new IPEndPoint(IPAddress.Parse(ipAndPort.Substring(0, pos)), port);
+		}
 
-                    string groupName;
-                    string fileName;
-                    SplitGroupNameAndFileName(fildid, out groupName, out fileName);
+		protected static bool WaitForFileExists(StorageNode node, string fileName, TimeSpan timeSpan)
+		{
+			DateTime begin = DateTime.Now;
+			bool fileExists = false;
+			TimeSpan sleepTime = TimeSpan.FromSeconds(5);
+			do
+			{
+				fileExists = FileExistsOnStorageNode(node, fileName);
+				if (fileExists)
+					return true;
 
-                    StorageNode[] nodes = FastDFSClient.QueryStorageNodesForFile(groupName, fileName);
-                    if (nodes == null)
-                    {
-                        throw new FDFSException(string.Format("Group {0} not found.", groupName));
-                    }
+				Thread.Sleep(sleepTime);
 
-                    StorageNode node = SelectStorageNode(nodes, fileName, timeoutSecs);
-                    if (node == null)
-                    {
-                        throw new FDFSException("node not available");
-                    }
-                    DoDownload(node, fileName, destFileName);
-                }
-                else if (string.Compare(op, "-q") == 0 ||
-                         string.Compare(op, "--query") == 0)
-                {
-                    string groupName;
-                    string fileName;
+			} while (!fileExists && DateTime.Now - begin < timeSpan);
+			return false;
+		}
 
-                    SplitGroupNameAndFileName(args[1], out groupName, out fileName);
+		protected static bool FileExistsOnStorageNode(StorageNode node, string fileName)
+		{
+			try
+			{
+				FastDFSClient.GetFileInfo(node, fileName);
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
 
-                    StorageNode[] nodes = FastDFSClient.QueryStorageNodesForFile(groupName, fileName);
-                    if (nodes == null)
-                    {
-                        throw new FDFSException(string.Format("Group {0} not found.", groupName));
-                    }
+		protected static void SplitGroupNameAndFileName(string fileId, out string groupName, out string fileName)
+		{
+			int index = fileId.IndexOf('/');
+			groupName = fileId.Substring(0, index);
+			fileName = fileId.Substring(index + 1);
+		}
 
-                    foreach (StorageNode node in nodes)
-                    {
-                        Console.WriteLine("Group:{0}\nServer:{1}\n", node.GroupName, node.EndPoint.ToString());
-                        if (FileExistsOnStorageNode(node, fileName))
-                        {
-                            Console.WriteLine("file exists.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("file doesn't exist.");
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Invalid options");
-                    Usage();
-                    return 1;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("ERROR:{0}", ex.Message);
-                return 1;
-            }
-            return 0;
-        }
+		protected virtual string OptionsToString()
+		{
+			var sb = new StringBuilder ();
+			sb.AppendFormat ("--timeout {0} ", _timeout);
+			_trackers.ForEach( t => sb.AppendFormat("--host {0} ", t) );
+			return sb.ToString ();
+		}
 
-        private static StorageNode SelectStorageNode(StorageNode[] nodes, string fileName, int timeoutSecs)
-        {
-            foreach (StorageNode node in nodes)
-            {
-                if (WaitForFileExists(node, fileName, TimeSpan.FromSeconds(timeoutSecs)))
-                    return node;
-            }
-            return null;
-        }
+		public override int? OverrideAfterHandlingArgumentsBeforeRun (string[] remainingArguments)
+		{
+			// use configuration if not specified in command line.
+			if (_trackers.Count == 0)
+				_trackers = TrackersFromSettings ();
+			return base.OverrideAfterHandlingArgumentsBeforeRun (remainingArguments);
+		}
+	}
 
-        private static bool WaitForFileExists(StorageNode node, string fileName, TimeSpan timeSpan)
-        {
-            DateTime begin = DateTime.Now;
-            bool fileExists = false;
-            TimeSpan sleepTime = TimeSpan.FromSeconds(5);
-            do
-            {
-                fileExists = FileExistsOnStorageNode(node, fileName);
-                if (fileExists)
-                    return true;
 
-                Thread.Sleep(sleepTime);
+	class DownloadCommand : FastDFSCommand
+	{
+		string _destFileName;
 
-            } while (!fileExists && DateTime.Now - begin < timeSpan);
-            return false;
-        }
+		public DownloadCommand()
+		{
+			IsCommand("download", "download file from FastDFS");
+			HasOption ("o|output=", "output filename",
+			           v => _destFileName = v);
+			HasAdditionalArguments (1, "<file_id>");
+		}
 
-        private static bool FileExistsOnStorageNode(StorageNode node, string fileName)
-        {
-            try
-            {
-                FastDFSClient.GetFileInfo(node, fileName);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
+		public override int Run(string[] remainingArguments)
+		{
+			string file_id = remainingArguments [0];
+			if (_dryrun) {
+				Console.WriteLine ("fdfsclient download {0} --output {1} {2}", OptionsToString(), _destFileName, file_id);
+				return 0;
+			}
+	
+			ConnectionManager.Initialize(_trackers);
+			string groupName = null;
+			string fileName  = null;
+			SplitGroupNameAndFileName(file_id, out groupName, out fileName);
 
-        private static void SplitGroupNameAndFileName(string fileId, out string groupName, out string fileName)
-        {
-            int index = fileId.IndexOf('/');
-            groupName = fileId.Substring(0, index);
-            fileName = fileId.Substring(index + 1);
-        }
+			StorageNode[] nodes = FastDFSClient.QueryStorageNodesForFile(groupName, fileName);
+			if (nodes == null)
+				throw new FDFSException(string.Format("Group {0} not found.", groupName));
 
-        private static void DoDownload(StorageNode node, string fileId, string destFileName)
-        {
-            //get metadata
-            IDictionary<string, string> metaData = FastDFSClient.GetMetaData(node, fileId);
+			StorageNode node = SelectStorageNode(nodes, fileName, _timeout);
+			if (node == null)
+				throw new FDFSException("node not available");
+			DoDownload(node, fileName, _destFileName);
+			return 0;
+		}
 
-            //download file
-            string destDir = Environment.CurrentDirectory;
-            string destName = metaData["filename"] + Path.GetExtension(fileId);
+		StorageNode SelectStorageNode(StorageNode[] nodes, string fileName, int timeoutSecs)
+		{
+			foreach (StorageNode node in nodes)
+			{
+				if (WaitForFileExists(node, fileName, TimeSpan.FromSeconds(timeoutSecs)))
+					return node;
+			}
+			return null;
+		}
 
-            if (!string.IsNullOrEmpty(destFileName))
-            {
-                string dir = Path.GetDirectoryName(destFileName);
-                if (!string.IsNullOrEmpty(dir))
-                    destDir = dir;
+		void DoDownload(StorageNode node, string fileId, string destFileName)
+		{
+			//get metadata
+			IDictionary<string, string> metaData = FastDFSClient.GetMetaData(node, fileId);
 
-                destName = Path.GetFileName(destFileName);
-            }
+			//download file
+			string destDir = Environment.CurrentDirectory;
+			string destName = metaData["filename"] + Path.GetExtension(fileId);
 
-            string fullName = FastDFSClient.DownloadFileEx(node, fileId, destDir, destName);
-            Console.WriteLine("{0}", fullName);
-        }
+			if (!string.IsNullOrEmpty(destFileName))
+			{
+				string dir = Path.GetDirectoryName(destFileName);
+				if (!string.IsNullOrEmpty(dir))
+					destDir = dir;
 
-        private static string DoUpload(StorageNode node, string fileName)
-        {
-            // upload file
-            string id = FastDFSClient.UploadFileByName(node, fileName);
+				destName = Path.GetFileName(destFileName);
+			}
 
-            // set name as metadata
-            Dictionary<string, string> metaData = new Dictionary<string, string>();
-            metaData["filename"] = Path.GetFileNameWithoutExtension(fileName);
-            FastDFSClient.SetMetaData(node, id, metaData);
-            return string.Format("{0}/{1}", node.GroupName, id);
-        }
+			string fullName = FastDFSClient.DownloadFileEx(node, fileId, destDir, destName);
+			Console.WriteLine("{0}", fullName);
+		}
+	}
 
-        private static void Usage()
-        {
-            Console.WriteLine("fdfsclient <-u|--upload> <file_to_upload>");
-            Console.WriteLine("fdfsclient <-d|--download> [--timeout secs] <file_id> [filename]");
-            Console.WriteLine("fdfsclient <-q|--query> <file_id>");
-        }
 
-        private static List<IPEndPoint> TrackersFromSettings()
-        {
-            List<IPEndPoint> trackers = new List<IPEndPoint>();
+	class UploadCommand : FastDFSCommand
+	{
+		public UploadCommand()
+		{
+			IsCommand("upload", "upload file to FastDFS");
+			HasAdditionalArguments (1, "<filename>");
+		}
 
-            foreach(string key in ConfigurationManager.AppSettings)
-            {
-                if (Regex.IsMatch(key, "Tracker[0-9]"))
-                {
-                    trackers.Add(CreateEndPoint(ConfigurationManager.AppSettings[key]));
-                }
-            }
-            return trackers;
-        }
+		public override int Run(string[] remainingArguments)
+		{
+			string filename = remainingArguments.FirstOrDefault();
+			if (_dryrun) {
+				Console.WriteLine ("fdfsclient upload {0} {1}", OptionsToString(), filename);
+				return 0;
+			}
+			ConnectionManager.Initialize(_trackers);
+			StorageNode node = FastDFSClient.GetStorageNode("group1");
+			string fileid = DoUpload(node, filename);
+			Console.WriteLine("{0}", fileid);
+			Console.WriteLine("http://{0}/{1}", _trackers[0].Address.ToString(), fileid);
+			return 0;
+		}
 
-        private static IPEndPoint CreateEndPoint(string ipAndPort)
-        {
-            int pos = ipAndPort.IndexOf(':');
+		string DoUpload(StorageNode node, string fileName)
+		{
+			// upload file
+			string id = FastDFSClient.UploadFileByName(node, fileName);
 
-            if (pos < 0)
-            {
-                // assume the default port if port isn't specified
-                return new IPEndPoint(IPAddress.Parse(ipAndPort), 22122);
-            }
+			// set name as metadata
+			Dictionary<string, string> metaData = new Dictionary<string, string>();
+			metaData["filename"] = Path.GetFileNameWithoutExtension(fileName);
+			FastDFSClient.SetMetaData(node, id, metaData);
+			return string.Format("{0}/{1}", node.GroupName, id);
+		}
+	}
 
-            int port = int.Parse(ipAndPort.Substring(pos + 1));
-            return new IPEndPoint(IPAddress.Parse(ipAndPort.Substring(0, pos)), port);
-        }
-    }
+
+	class QueryCommand : FastDFSCommand
+	{
+		public QueryCommand()
+		{
+			IsCommand("query", "query file on FastDFS");
+			HasAdditionalArguments (1, "<fild_id>");
+		}
+
+		public override int Run(string[] remainingArguments)
+		{
+			string file_id = remainingArguments [0];
+			if (_dryrun) {
+				Console.WriteLine ("fdfsclient query {0} {1}", OptionsToString(), file_id);
+				return 0;
+			}
+			ConnectionManager.Initialize(_trackers);
+
+			string groupName = null;
+			string fileName  = null;
+			SplitGroupNameAndFileName(file_id, out groupName, out fileName);
+
+			StorageNode[] nodes = FastDFSClient.QueryStorageNodesForFile(groupName, fileName);
+			if (nodes == null) {
+				throw new FDFSException(string.Format("Group {0} not found.", groupName));
+			}
+
+			foreach (StorageNode node in nodes)
+			{
+				Console.WriteLine("Group:{0}\nServer:{1}\n", node.GroupName, node.EndPoint.ToString());
+				if (FileExistsOnStorageNode(node, fileName))
+				{
+					Console.WriteLine("file exists.");
+				}
+				else
+				{
+					Console.WriteLine("file doesn't exist.");
+				}
+			}
+			return 0;
+		}
+	}
+
+
+	class Program
+	{
+		public static int Main (string[] args)
+		{
+			// locate any commands in the assembly (or use an IoC container, or whatever source)
+			var commands = GetCommands();
+
+			// then run them.
+			return ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
+		}
+
+		public static IEnumerable<ConsoleCommand> GetCommands()
+		{
+			return ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof(Program));
+		}
+	}
 }
